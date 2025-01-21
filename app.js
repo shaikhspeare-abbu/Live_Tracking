a6c534b9a00d04ce0e28a58ccd7c96a294d3fabf
Abdullah <abdullahsheikh0945@gmail.com>
2025-01-21 22:20:12 +0530

    Interface added

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const http = require("http");
 
 const socketio = require("socket.io");
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 const io = socketio(server);
-app.get("/", function (req, res){
+
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname, "public")));
+
+
+app.get("/", function(req, res){
     res.send("hey");
 });
 
-app.listen(3000);
+server.listen(3000);
