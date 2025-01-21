c52e12169bbc949c9474e1bf69be0491cbb37a6b
Abdullah <abdullahsheikh0945@gmail.com>
2025-01-21 23:06:35 +0530

    added attribution

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const path = require("path");
+
 const http = require("http");
 
 const socketio = require("socket.io");
@@ -11,9 +13,11 @@ const io = socketio(server);
 app.set("view engine", "ejs");
 app.set(express.static(path.join(__dirname, "public")));
 
-
+io.on("connection", function(socket){
+    console.log("connected");
+});
 app.get("/", function(req, res){
-    res.send("hey");
+    res.render("index");
 });
 
 server.listen(3000);
