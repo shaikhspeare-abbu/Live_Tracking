dbf5ebf49a7668584f2bbbf716ccb647947f66f2
Abdullah <abdullahsheikh0945@gmail.com>
2025-01-21 21:55:43 +0530

    Cs

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const http = require("http");
 
-const socketio = require("socketio");
+const socketio = require("socket.io");
 
 const server = http.createServer(app)
+
+const io = socketio(server);
 app.get("/", function (req, res){
     res.send("hey");
 });
